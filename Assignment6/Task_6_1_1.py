9f3ba4f6e43114530d8109c740768b6d1876809a
Jonas Papmeier <1papmeie@informatik.uni-hamburg.de>
2016-07-12 13:45:52 +0200

    Task6.1.1+6.1.2 update

--- Assignment6/Task_6_1_1.py
+++ Assignment6/Task_6_1_1.py
@@ -67,25 +67,54 @@ def transJoint(l,t):
 def endEffPosition(t1,t2):
     return np.dot(np.dot(np.dot(coordSys,base),transJoint(l1,t1)),transJoint(l2,t2))
 
+obstacles = []
+
 #Define the first 2 circular obstacles
 o1 = Circle(np.array([270,620,1]),50)
+obstacles.append(o1)
 o2 = Circle(np.array([250,200,1]),200)
+obstacles.append(o2)
 
-#Stepsize for joint angle
-resolution = 360
-#initialize array beforehand
-Cspace = np.zeros((resolution,resolution))
-i = 0
-for t1 in np.linspace(0,math.pi*2,resolution,False):
-    j = 0
-    for t2 in np.linspace(0,math.pi*2,resolution,False):
-        #Calculate End Effector once, only last column needed for x,y coordinates
-        curCartesianPosition = endEffPosition(t1,t2)[:,2]
-        #TCP Missing?
-        Cspace[i][j] = o1.pointInside(curCartesianPosition) + o2.pointInside(curCartesianPosition) * 0.5 #Scaling by 0.5 changes color in plot 
-        j = j+1
-    i= i+1
-    
-#plot as 2D color plot
-matplotlib.pyplot.pcolor(Cspace)
+start = Circle(np.array([900,500,1]),10)
+goal1 = Circle(np.array([580,150,1]),10)
+goal2 = Circle(np.array([230,470,1]),10)
+obstacles.append(start)
+obstacles.append(goal1)
+obstacles.append(goal2)
+
+
+def plotCspace(figNum,resolution):
+    #Stepsize for joint angle
+    resolution_cs = resolution
+    #initialize array beforehand
+    Cspace = np.zeros((resolution_cs,resolution_cs))
+    i = 0
+    for t1 in np.linspace(0,math.pi*2,resolution_cs,False):
+        j = 0
+        for t2 in np.linspace(0,math.pi*2,resolution_cs,False):
+            #Calculate End Effector once, only last column needed for x,y coordinates
+            curCartesianPosition = endEffPosition(t1,t2)[:,2]
+            #TCP Missing?
+            Cspace[i][j] = reduce(lambda x, y: x*2+y,list(map(lambda o: o.pointInside(curCartesianPosition), obstacles)))
+            j = j+1
+        i= i+1
+            
+    #plot as 2D color plot
+    matplotlib.pyplot.figure(figNum)
+    matplotlib.pyplot.pcolor(Cspace)
+
+def plotWspace(figNum,resolution,x,y):
+    resolution_ws = resolution
+    ws_bounds = (x,y)
+    Wspace = np.zeros((resolution_ws,resolution_ws))
+    i = 0
+    for t1 in np.linspace(0,ws_bounds[0],resolution_ws,False):
+        j = 0
+        for t2 in np.linspace(0,ws_bounds[1],resolution_ws,False):
+            Wspace[i][j] = reduce(lambda x, y: x*2+y,list(map(lambda o: o.pointInside((t1,t2,1)), obstacles)))
+            j = j+1
+        i = i+1
+
+    matplotlib.pyplot.figure(figNum)
+    matplotlib.pyplot.pcolor(Wspace)
     
